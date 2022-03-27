91e59b200943d0b9fd3d710a489f125c24cada93
lnewsom <lara@laranewsom.com>
2022-03-27 17:25:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-pvukck
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pvukck)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('App');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
